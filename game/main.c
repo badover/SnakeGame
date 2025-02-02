64ce5c63e68d6c4e5538a3b5fbdbf11a026018ca
badover <155159993+badover@users.noreply.github.com>
2025-02-02 11:21:57 -0500

    Update main.c

--- game/main.c
+++ game/main.c
@@ -29,7 +29,6 @@ void initGame(Snake *snake, Food *food){
 
     food->position.x = rand() % WIDTH;
     food->position.y = rand() % HEIGHT;
-
 }
 
 void drawGame(Snake *snake, Food *food){
@@ -48,10 +47,9 @@ void drawGame(Snake *snake, Food *food){
         mvprintw(snake->body[i].y, snake->body[i].x, "O");
     }
 
-    mvprintw(food->position.y, food->position.x,"X");
+    mvprintw(food->position.y, food->position.x, "X");
 
     refresh();
-
 }
 
 void updateGame(Snake *snake, Food *food, int *gameOver){
@@ -60,26 +58,25 @@ void updateGame(Snake *snake, Food *food, int *gameOver){
         snake->body[0].y + snake->direction.y
     };
 
-    for(int i = snake-> length - 1; i > 0; i--){
+    for(int i = snake->length - 1; i > 0; i--){
         snake->body[i] = snake->body[i-1];
     }
     
     snake->body[0] = nextPosition;
 
-    //chek walls and self
+    // Check walls and self
     if (nextPosition.x < 0 || nextPosition.x >= WIDTH || nextPosition.y < 0 || nextPosition.y >= HEIGHT) {
         *gameOver = 1;
     }
 
-    //food
-     if (nextPosition.x == food->position.x && nextPosition.y == food->position.y) {
+    // Food
+    if (nextPosition.x == food->position.x && nextPosition.y == food->position.y) {
         snake->length++;  
         food->position.x = rand() % WIDTH;  
         food->position.y = rand() % HEIGHT; 
     }
 
-    //check herself
-
+    // Check self
     for (int i = 1; i < snake->length; i++) {
         if (snake->body[i].x == nextPosition.x && snake->body[i].y == nextPosition.y) {
             *gameOver = 1;
@@ -99,61 +96,75 @@ int main(){
     srand(time(0));
 
     initscr();
-    curs_set(1);
+    curs_set(0);  // Hide cursor
     keypad(stdscr, TRUE);
     noecho();
     timeout(100);
 
-    Snake snake;
-    Food food;
     int gameOver = 0;
+    int menuChoice = 0;
+    
+    while (1) {
+        displayMenu();
 
-    initGame(&snake, &food);
+        int input = getch();
+        switch (input) {
+            case '1':  // Start game
+                {
+                    Snake snake;
+                    Food food;
+                    gameOver = 0;
+                    initGame(&snake, &food);
 
-    while (!gameOver){
-        drawGame(&snake, &food);
+                    while (!gameOver) {
+                        drawGame(&snake, &food);
 
-        int input  = getch();
-        switch(input){
-            case KEY_UP:
-                if (snake.direction.y == 0){
-                    snake.direction.x = 0;
-                    snake.direction.y = -1;
+                        int input = getch();
+                        switch(input) {
+                            case KEY_UP:
+                                if (snake.direction.y == 0){
+                                    snake.direction.x = 0;
+                                    snake.direction.y = -1;
+                                }
+                                break;
+
+                            case KEY_DOWN:
+                                if (snake.direction.y == 0){
+                                    snake.direction.x = 0;
+                                    snake.direction.y = 1;
+                                }
+                                break;
+                            
+                            case KEY_LEFT:
+                                if (snake.direction.x == 0){
+                                    snake.direction.x = -1;
+                                    snake.direction.y = 0;
+                                }
+                                break;
+
+                            case KEY_RIGHT:
+                                if (snake.direction.x == 0){
+                                    snake.direction.x = 1;
+                                    snake.direction.y = 0;
+                                }
+                                break;
+
+                            case 'q':  // Quit game
+                                gameOver = 1;
+                                break; 
+                        }
+
+                        updateGame(&snake, &food, &gameOver);
+                    }
                 }
                 break;
 
-            case KEY_DOWN:
-                if (snake.direction.y == 0){
-                    snake.direction.x = 0;
-                    snake.direction.y = 1;
-                }
-                break;
-            
-            case KEY_LEFT:
-                if (snake.direction.x == 0){
-                    snake.direction.x = -1;
-                    snake.direction.y = 0;
-                }
-                break;
+            case '2':  // Exit
+                endwin();
+                return 0;
 
-            case KEY_RIGHT:
-                if (snake.direction.x == 0){
-                    snake.direction.x = 1;
-                    snake.direction.y = 0;
-                }
+            default:
                 break;
-
-            case 'q':
-                gameOver = 1;
-                break; 
         }
-
-        updateGame(&snake, &food, &gameOver);
-    }  
-
-
-        refresh();
-        //getch();
-        endwin();
-        return 0;
+    }
 }
